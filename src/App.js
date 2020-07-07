40511e255c9e771d4a2b1c30d0e9a7c80c1115d8
Clay Morris <clay@phiguru.net>
2020-07-07 21:40:23 +0000

    yeah

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React! biiiiiiiiiitch!
+          TIDDIES!
         </a>
       </header>
     </div>
